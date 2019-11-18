6806935d8800d535ed474934786796f1b1502d3d
tianyu <tianyu5177@sina.com>
2019-11-18 16:28:26 +0800

    init

---
+++ src/components/hello/hello.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+//const {Component} = React
+
+export default class Hello extends Component{
+  render(){
+    return (
+      <div>我是hello组件</div>
+    )
+  }
+}
---
+++ src/components/peiqi/peiqi.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+
+export default class Peiqi extends Component{
+  render(){
+    return (
+      <div>佩奇是一头猪，全家都是猪</div>
+    )
+  }
+}
